f9a2dae598d175de74c3228047320a7261a5f402
maniharshitha19 <mani.harshitha2006@gmail.com>
2026-08-13 14:47:52 +0530

    Raised an error

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial {
     public static int compute(int n) {
         if(n < 0) throw new IllegalArgumentException("Negative numbers are not allowed");
-        long result = 1;
+        long result = 0;
         for(int i = 2; i <= n; i++) {
             result *= i;
         }
